dcc95e3b25d82af1c5d3194227dc6f56f5a70486
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-03-13 18:52:58 +0300

    Add nix support & test changes.

--- math1/src/Main.hs
+++ math1/src/Main.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Main where
+
+import qualified Data.Aeson   as A
+import           Data.Functor ()
+import qualified Data.Maybe   (maybe)
+
+anothershit = undefined
+someCrazyShit = anothershit
+
 main :: IO ()
-main = undefined
+main = do
+    putStrLn "lol sasi"
+
+    return ()
