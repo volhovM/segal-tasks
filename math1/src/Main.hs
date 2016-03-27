e2bc6108d7633cdc10421f7401c7d5d5e0314731
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-03-27 23:01:07 +0300

    Remove gtk dependency.

--- math1/src/Main.hs
+++ math1/src/Main.hs
@@ -3,10 +3,8 @@
 module Main where
 
 import qualified Gauss as G
-import qualified Glade as GL
-import qualified Types as T
 
 main :: IO ()
 main = do
     putStrLn "Still not implemented, meh"
-    undefined G.main (GL.MainWindow  undefined)
+    undefined G.main
